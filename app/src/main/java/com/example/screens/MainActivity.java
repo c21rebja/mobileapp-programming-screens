0ba7134e8c648be4f0fd70e5bb6067324a52e73e
c21rebja <c21rebja@student.his.se>
2022-04-26 08:54:00 +0200

    added logs for main activity lifecycle stages

--- app/src/main/java/com/example/screens/MainActivity.java
+++ app/src/main/java/com/example/screens/MainActivity.java
@@ -3,12 +3,38 @@ package com.example.screens;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onPostResume() {
+        super.onPostResume();
+        Log.d("===", "Main activity resumed.");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("===", "Main activity started.");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("===", "Main activity stopped.");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("===", "Main activity destroyed.");
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d("===", "Main activity created.");
     }
 }
