c6e4790db4ab24616db9d572d9ff7871f2bfc96f
c21rebja <c21rebja@student.his.se>
2022-04-26 09:00:12 +0200

    logged lifecycle of second activity

--- app/src/main/java/com/example/screens/DisplayActivity.java
+++ app/src/main/java/com/example/screens/DisplayActivity.java
@@ -3,12 +3,38 @@ package com.example.screens;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class DisplayActivity extends AppCompatActivity {
+
+    @Override
+    protected void onPostResume() {
+        super.onPostResume();
+        Log.d("===", "Display activity resumed.");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("===", "Display activity started.");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("===", "Display activity stopped.");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("===", "Display activity destroyed.");
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display);
+        Log.d("===", "Display activity created.");
     }
 }
